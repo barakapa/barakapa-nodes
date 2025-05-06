c215ddcbdae6fced32056be352cd62e0a6e5e8e5
barakapa <barakapa@proton.me>
2025-05-06 17:41:40 +0800

    Fix typehints

--- nodes/directory_image_counter.py
+++ nodes/directory_image_counter.py
@@ -3,7 +3,7 @@ from typing import Optional
 
 from folder_paths import get_output_directory
 
-from .utils import InputDict, search_and_replace
+from .utils import InputDict, Json, search_and_replace
 
 IMAGE_EXTS: set[str] = {
     '.png',
@@ -24,19 +24,19 @@ class DirectoryImageCounterNode:
                 'extra_pnginfo': 'EXTRA_PNGINFO'
             },
         }
- 
+
     RETURN_TYPES: tuple[str, ...] = ('INT', 'STRING')
     RETURN_NAMES: tuple[str, ...] = ('int', 'string')
- 
+
     FUNCTION: str = 'count_dir_images'
- 
+
     # OUTPUT_NODE: bool = False
- 
+
     CATEGORY: str = 'custom/Directory Image Counter'
 
     @classmethod
     def IS_CHANGED(self, **_) -> float:
-        '''The node will always be re executed if any of the inputs change but
+        '''The node will always be re-executed if any of the inputs change but
         this method can be used to force the node to execute again even when the inputs don't change.
         You can make this node return a number or a string. This value will be compared to the one
         returned the last time the node was executed, if it is different the node will be executed again.
@@ -45,9 +45,9 @@ class DirectoryImageCounterNode:
         '''
         # NaN is never equal to any value
         return float('nan')
- 
+
     def count_dir_images(
-        self, directory_name: str, prompt: Optional[str] = None, extra_pnginfo: Optional[str] = None
+        self, directory_name: str, prompt: Optional[str | Json] = None, extra_pnginfo: Optional[str | Json] = None
     ) -> tuple[int, str]:
         dir_name: str = search_and_replace(directory_name, prompt, extra_pnginfo)
         full_output_folder: str = path.join(self.output_dir, dir_name)
