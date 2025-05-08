3695508ef1419627162cc02ed2541e09b5547eda
barakapa <barakapa@proton.me>
2025-05-08 17:26:38 +0800

    Rework search_and_replace() function
    * Look for node references in this order: ID, S&R value, display name,
      type
    
    * Throw more errors from search_and_replace()
    
    * Fix widget_value being converted into string incorrectly

--- custom_nodes/utils.py
+++ custom_nodes/utils.py
@@ -4,8 +4,9 @@ from datetime import datetime
 from json import dumps, loads
 from math import isclose
 from re import Match, findall, finditer
-from typing import Optional, TypeAlias
+from typing import Callable, Optional, TypeAlias, TypeVar
 
+from nodes import NODE_DISPLAY_NAME_MAPPINGS
 from typing_extensions import Self
 
 # This value is used for comparing between two floating-point numbers.
@@ -26,7 +27,7 @@ InputDict: TypeAlias = dict[str, dict[str, str | tuple[str | list[str], dict[str
 class JsonOpt:
     '''Represents an optional JSON object. Allows for .get method chaining.'''
 
-    def __init__(self, json: Optional[Json] = None):
+    def __init__(self, json: Optional[Json] = None) -> None:
         self.json: Optional[Json] = json
 
     def get(self, key: str) -> Self:
@@ -46,13 +47,75 @@ class JsonOpt:
     def to_str(self) -> Optional[str]:
         if isinstance(self.json, str):
             return self.json
-        elif isinstance(self.json, int):
+        elif self.json:
             return str(self.json)
         else:
             return None
 
     def is_none(self) -> bool:
         return self.json is None
+
+def find_node_id(node: JsonOpt) -> str:
+    '''Attempts to retrieve a node's ID.'''
+    node_id: Optional[str] = node.get('id').to_str()
+    if not node_id:
+        raise ValueError('ComfyUI node has missing ID!')
+    return node_id
+
+def find_node_display_name(node: JsonOpt) -> str:
+    '''Attempts to retrieve a node's display name in the following order.
+    1. Get node.title (custom display name added by user).
+    2. Attempt to map node.type to the node's default display name.
+    3. If no display name exists, return node.type (the internal node type name).
+    '''
+    title: Optional[str] = node.get('title').to_str()
+    if title:
+        return title
+
+    node_type: Optional[str] = node.get('type').to_str()
+    if not node_type:
+        raise ValueError(f'ComfyUI node has missing type!')
+
+    if node_type in NODE_DISPLAY_NAME_MAPPINGS:
+        return NODE_DISPLAY_NAME_MAPPINGS[node_type]
+    else:
+        return node_type
+
+def find_node_snr(node: JsonOpt) -> Optional[str]:
+    '''Attempts to retrieve a node's custom search and replace value specified by the user.'''
+    return node.get('properties').get('Node name for S&R').to_str()
+
+T = TypeVar('T')
+def map_unique_value_from_node(
+    node: JsonOpt,
+    retrieve_fn: Callable[[JsonOpt], Optional[T]],
+    value_to_id_map: dict[T, str],
+    duplicates_set: set[T],
+    is_fail_on_missing: bool = True
+) -> None:
+    '''Attempts to retrieve a certain value of type T from a node using "retrieve_fn".
+    If the value is None and "is_fail_on_missing" is set to True, raise an error.
+    If the value is unique, store it as a key in "id_map" for the node's ID,
+    otherwise, delete the value from "id_map" and store it in "duplicates_set".
+    '''
+
+    value: Optional[T] = retrieve_fn(node)
+    if not value:
+        if is_fail_on_missing:
+            raise ValueError('Failed to retrieve unique value!')
+        else:
+            return
+
+    node_id: str = find_node_id(node)
+    if value not in value_to_id_map and value not in duplicates_set:
+        value_to_id_map[value] = node_id
+    elif value in value_to_id_map:
+        # print(f'Unique Value Warning: Node #{value_to_id_map[value]} and '
+        #       f'#{node_id} share the same retrieved value  "{value}"!')
+        del value_to_id_map[value]
+        duplicates_set.add(value)
+    # else: # value in duplicates_set
+    #     print(f'Warning: Node #{node_id} has duplicate retrieved value "{value}"!')
 
 def search_and_replace(text: str, prompt: Optional[str | Json], extra_pnginfo: Optional[str | Json]) -> str:
     '''Replaces date and other S&R tags in a string with the correct values.'''
@@ -120,50 +183,77 @@ def search_and_replace(text: str, prompt: Optional[str | Json], extra_pnginfo: O
     if not nodes:
         return text
 
-    # Map from "Node name for S&R" to id in the workflow
-    node_to_id_map: dict[str, str] = {}
+    # Map of unique node display names to their IDs
+    node_name_map: dict[str, str] = {}
+    # Map of "Node name for S&R" values to their IDs
+    node_snr_map: dict[str, str] = {}
+    # Map of unique node types to their IDs
+    node_type_map: dict[str, str] = {}
+
+    # Set of duplicated display names
+    node_name_duplicates: set[str] = set()
+    # Set of duplicated S&R names
+    node_snr_duplicates: set[str] = set()
+    # Set of duplicated node types
+    node_type_duplicates: set[str] = set()
+
+    # Set of all unique node IDs
+    node_ids: set[str] = set()
+
     for node in nodes:
-        node_name: Optional[str] = node.get('properties').get('Node name for S&R').to_str()
-        node_id: Optional[str] = node.get('id').to_str()
-        if node_name and node_id:
-            node_to_id_map[node_name] = node_id
-        else: 
-            return text
+        node_id: str = find_node_id(node)
+        if node_id in node_ids:
+            raise ValueError('Duplicate node IDs in ComfyUI workflow graph!')
+        node_ids.add(node_id)
+
+        map_unique_value_from_node(node, find_node_display_name, node_name_map, node_name_duplicates)
+        map_unique_value_from_node(node, find_node_snr, node_snr_map, node_snr_duplicates, False)
+        map_unique_value_from_node(node, lambda n: n.get('type').to_str(), node_type_map, node_type_duplicates)
 
     # Find all patterns in the text that need to be replaced
     patterns: list[str] = findall(r'%([^%]+)%', text)
     pattern: str
     for pattern in patterns:
-        # Split the pattern to get the node name and widget name
-        node_name_key: str
+        # Split the pattern to get the node key and widget name
+        node_key: str
         widget_name: str
-        node_name_key, widget_name = pattern.split('.')
+        node_key, widget_name = pattern.split('.')
 
-        # Find the id for this node name
+        if node_key in node_snr_duplicates or node_key in node_name_duplicates or node_key in node_type_duplicates:
+            raise ValueError(f'Ambiguous node key "{node_key}" for search and replace!')
+
+        # Find the ID for a given node key
         node_id_str: str
-        if node_name_key in node_to_id_map:
-            node_id_str = node_to_id_map[node_name_key]
+        if node_key in node_ids:        # 1. Check if node_key is node's ID
+            node_id_str = node_key
+        elif node_key in node_snr_map:  # 2. Check if node_key is S&R value
+            node_id_str = node_snr_map[node_key]
+        elif node_key in node_name_map: # 3. Check if node_key is unique node display name
+            node_id_str = node_name_map[node_key]
+        elif node_key in node_type_map: # 4. Check if node_key is unique node type
+            node_id_str = node_type_map[node_key]
         else:
-            print(f'No node with name {node_name_key} found.')
-            # check if user entered id instead of node name
-            if node_name_key in node_to_id_map.values():
-                node_id_str = node_name_key
-            else:
-                continue
+            print(f'No node with ID, or unique name, or unique type "{node_key}" found.')
+            continue
 
         # Find the value of the specified widget in prompt JSON
         node_from_id: JsonOpt = prompt_jsonopt.get(node_id_str)
         if node_from_id.is_none():
-            print(f'No node with id {node_id_str} found.')
-            continue
+            raise ValueError(f'No node with ID {node_id_str} found in prompt!')
 
         widget_value: JsonOpt = node_from_id.get('inputs').get(widget_name)
         if widget_value.is_none():
-            print(f'No widget with name {widget_name} found for node {node_name}.')
-            continue
+            raise ValueError(f'No input with name {widget_name} found for node '
+                             '"{node_key}" with ID #{node_id_str}!')
 
-        # Replace the pattern in the text
-        text = text.replace(f'%{pattern}%', str(widget_value))
+        # Finally, replace the pattern in the text
+        widget_value_str: Optional[str] = widget_value.to_str()
+        replace_value: str = ''
+        if not widget_value_str:
+            print(f'Warning: Search and replace value is falsy: "{str(widget_value.json)}"!')
+        else:
+            replace_value = widget_value_str
+        text = text.replace(f'%{pattern}%', replace_value)
 
     return text
 
