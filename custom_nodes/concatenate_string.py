67c53c68852c4daae3c66cb7c62e02b850725993
barakapa <barakapa@proton.me>
2025-05-09 00:50:03 +0800

    Add Concatenate String node

---
+++ custom_nodes/concatenate_string.py
@@ -0,0 +1,20 @@
+from .utils import InputDict
+
+
+class ConcatenateStringNode:
+    @classmethod
+    def INPUT_TYPES(cls) -> InputDict:
+        return {
+            'required': {
+                'str_1': ('STRING', {'default': ''}),
+                'str_2': ('STRING', {'default': ''}),
+            },
+        }
+
+    RETURN_TYPES: tuple[str, ...] = ('STRING',)
+    RETURN_NAMES: tuple[str, ...] = ('output_str',)
+    FUNCTION: str = 'concatenate_string'
+    CATEGORY: str = 'custom'
+    
+    def concatenate_string(self, str_1: str, str_2: str) -> tuple[str]:
+        return (str_1 + str_2,)
